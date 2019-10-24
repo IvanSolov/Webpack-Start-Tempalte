735783db936b755c4ddf6e0062e1ac559345be31
IvanCavdar <ivancavdar25@gmail.com>
2019-10-24 19:07:05 +0300

    Remove dist before build

--- webpack.build.conf.js
+++ webpack.build.conf.js
@@ -1,12 +1,15 @@
 const merge = require('webpack-merge')
 const baseWebpackConfig = require('./webpack.base.conf')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const buildWebpackConfig = merge(baseWebpackConfig, {
    mode: 'production',
 
-   plugins: []
+   plugins: [
+      new CleanWebpackPlugin()
+   ]
 });
 
-module.exports = new Promise((resolve, reject) => {
+module.exports = new Promise((resolve) => {
    resolve(buildWebpackConfig)
 })
